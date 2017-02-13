e3f4146509d74582401c4b134a0ea0ca47ab77e2
Ali Tejani <Ali Tejani>
2017-02-13 16:49:40 -0600

    sine function

--- Lab3/ClockMain.c
+++ Lab3/ClockMain.c
@@ -8,6 +8,8 @@
 // TA: Lavanya
 // Last Revision: 2/9/2017
 
+#define ADC_READ_HOUR			((BaseTime/100 + ADCValue*24/4096) % 24) * 100
+#define ADC_READ_MINUTE		((BaseTime%100 + ADCValue*60/4096) % 60)
 
 #include <stdint.h>
 #include "ST7735.h"
@@ -19,59 +21,88 @@
 #include "ADCSWTrigger.h"
 #include "../inc/tm4c123gh6pm.h"
 
-// state values
-const uint32_t DISPLAY_CLOCK = 0;
-const uint32_t SET_TIME_HOUR = 1;
-const uint32_t SET_TIME_MINUTE = 2;
-const uint32_t SET_ALARM_HOUR = 3;
-const uint32_t SET_ALARM_MINUTE = 4;
 
-static uint32_t State = DISPLAY_CLOCK;		// current state
+const uint8_t[] SIN_FIXED = [0, 10, 20, 30, 40, 49, 58, 66, 74, 80,
+														86, 91, 95, 97, 99, 100, 99, 97, 95, 91,
+														86, 80, 74, 66, 58, 49, 40, 30, 20, 10,
+														0, -10, -20, -30, -40, -49, -58, -66, -74, -80,
+														-86, -91, -95, -97, -99, -100, -99, -97, -95, -91,
+														-86, -80, -74, -66, -58, -49, -40, -30, -20, -10];
+
+// state values
+const uint32_t STANDARD_CLOCK = 0;
+const uint32_t MILITARY_CLOCK = 1;
+const uint32_t SET_TIME_HOUR = 2;
+const uint32_t SET_TIME_MINUTE = 3;
+const uint32_t SET_ALARM_HOUR = 4;
+const uint32_t SET_ALARM_MINUTE = 5;
+
+static uint32_t State;		// current state
 static uint32_t AlarmOn = 0;							// 1 if alarm is on
 static uint32_t AlarmTime = 0;						// time of alarm
 static volatile uint32_t ADCValue = 0;		// adc value
+static uint32_t HomeState;
+static uint32_t BaseTime;
+static uint32_t NewTime;
 extern uint32_t AtomicTime;								// current time
 
 // start set time state, or move to next state
-void button1SetTime(void) { 
-	if(State != SET_TIME_HOUR && State != SET_TIME_MINUTE) {
-		State = SET_TIME_HOUR;
+void Button1SetTime(void) { 
+	if(State == SET_TIME_HOUR) {
+		NewTime = ADC_READ_HOUR;
+		State = SET_TIME_MINUTE;
+	} else if (State == SET_TIME_MINUTE) {
+		NewTime += ADC_READ_MINUTE;
+		AtomicTime = NewTime;
+		State = HomeState;
 	} else {
-		State = DISPLAY_CLOCK;
+		BaseTime = AtomicTime;
+		State = SET_TIME_HOUR;
 	}
 }
 
 // start set alarm state, or move to next state
-void button2SetAlarm(void) {
-	if(State != SET_ALARM_HOUR && State != SET_ALARM_MINUTE) {
-		State = SET_ALARM_HOUR;
+void Button2SetAlarm(void) {
+	if(State == SET_ALARM_HOUR) {
+		NewTime = ADC_READ_HOUR;
+		State = SET_ALARM_MINUTE;
+	} else if (State == SET_ALARM_MINUTE) {
+		NewTime += ADC_READ_MINUTE;
+		AlarmTime = NewTime;
+		State = HomeState;
 	} else {
-		State = DISPLAY_CLOCK;
+		BaseTime = AlarmTime;
+		State = SET_ALARM_HOUR;
 	}
 }
 
 // enable/disable alarm
-void buttton3ToggleAlarm(void) {
+void Buttton3ToggleAlarm(void) {
 	AlarmOn = 1 - AlarmOn;
 }
 
 // move to next state
-void button4ChangeMode(void) {
-	if(State == SET_TIME_HOUR) {
-		State = SET_TIME_MINUTE;
-	} else if(State == SET_TIME_MINUTE) {
-		State = DISPLAY_CLOCK;
-		// set AtomicTime
-	} else if(State == SET_ALARM_HOUR) {
-		State = SET_ALARM_MINUTE;
-	} else if(State == SET_ALARM_MINUTE) {
-		State = DISPLAY_CLOCK;
-		// set AlarmTime
+void Button4ChangeMode(void) {
+	if(State == STANDARD_CLOCK) {
+		State = MILITARY_CLOCK;
+	} else if(State == MILITARY_CLOCK) {
+		State = STANDARD_CLOCK;
+	} else {
+		State = 1 - HomeState;
 	}
 }
 
+const uint32_t X_CLOCK_CENTER;
+const uint32_t Y_CLOCK_CENTER;
+const uint32_t HOUR_HAND_LENGTH;
+const uint32_t MINUTE_HAND_LENGTH;
+
+void DrawHourHand(uint32_t time) {
+	uint32_t angle = (((time/100) % 12)*60 + time%100)/12;
+}
+
 // draw clock and lines
-void drawClock(char* title, uint32_t time) {
+void DrawClock(char* title, uint32_t time) {
 	// clear old values 
 	
 	// draw title
@@ -89,7 +120,40 @@ void drawClock(char* title, uint32_t time) {
 	ST7735_OutUDec(time/100);
 	ST7735_OutString(":");
 	ST7735_OutUDec(time%100);
+	// draw standard vs military
+	if(HomeState == STANDARD_CLOCK) {
+	} else {
+	}
 	
+}
+
+void DisplayClock(void) {
+	uint32_t time = AtomicTime;
+	char* screen;
+	if(State == STANDARD_CLOCK) {					// draw clock with time from systick
+		screen = "Clock";
+	} else if(State == MILITARY_CLOCK) {	// draw clock with time from systick
+		screen = "Clock";
+	} else if(State == SET_TIME_HOUR) {		// draw clock with time from start time 
+																				// from systick offset by adc
+		uint32_t time2 = ADC_READ_HOUR;
+		time = time2 + time % 100;
+		screen = "Set Time - Hours";
+	} else if(State == SET_TIME_MINUTE) {	// draw clock with time from start time 
+																				// from systick offset by adc
+		uint32_t time2 = ADC_READ_MINUTE;
+		time = (time/100)*100 + time2;
+		screen = "Set Time - Minutes";
+	} else if(State == SET_ALARM_HOUR) {	// draw clock with alarm time offset by adc
+		uint32_t time2 = ADC_READ_HOUR;
+		time = time2 + AlarmTime % 100;
+		screen = "Set Alarm - Hours";
+	} else if(State == SET_ALARM_MINUTE) {	// draw clock with alarm time offset by adc
+		uint32_t time2 = ADC_READ_MINUTE;
+		time = (AlarmTime/100)*100 + time2;
+		screen = "Set Alarm - Minutes";
+	}
+	DrawClock(screen,time);
 }
 
 int main(void) {
@@ -97,35 +161,14 @@ int main(void) {
 	SysTick_Init();
 	Speaker_Init();
 	ST7735_InitR(INITR_REDTAB);
-	Switch_Init(button1SetTime, button2SetAlarm, buttton3ToggleAlarm,
-			button4ChangeMode);
+	Switch_Init(Button1SetTime, Button2SetAlarm, Buttton3ToggleAlarm,
+			Button4ChangeMode);
 	ADC0_InitSWTriggerSeq3_Ch9();
+	HomeState = STANDARD_CLOCK;
+	State = HomeState;
 	
 	while(1) {
-		uint32_t time = AtomicTime;
-		if(State == DISPLAY_CLOCK) {					// draw clock with time from systick
-			drawClock("Clock", time);
-		} else if(State == SET_TIME_HOUR) {		// draw clock with time from start time 
-																					// from systick offset by adc
-			// sample adc
-			uint32_t hour = ADCValue / 24;
-			time = hour *100 + time%100;
-			drawClock("Set Time - Hours", time);
-		} else if(State == SET_TIME_MINUTE) {	// draw clock with time from start time 
-																					// from systick offset by adc
-			// sample adc
-			uint32_t minute = ADCValue / 60;
-			time = time /100 + minute;
-			drawClock("Set Time - Minutes",time);
-		} else if(State == SET_ALARM_HOUR) {	// draw clock with alarm time offset by adc
-			uint32_t hour = ADCValue / 24;
-			time = hour /100 + AlarmTime%100;
-			drawClock("Set Alarm - Hours",time);
-		} else if(State == SET_ALARM_MINUTE) {	// draw clock with alarm time offset by adc
-			uint32_t minute = ADCValue / 60;
-			time = AlarmTime /100 + minute;
-			drawClock("Set Alarm - Minutes",time);
-		}
+		DisplayClock();
 	}
 }
 
